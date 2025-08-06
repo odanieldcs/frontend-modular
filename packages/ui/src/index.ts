81219d4b22ad01fd58f360cba4aa798828714b59
Daniel Castro <2968031+odanieldcs@users.noreply.github.com>
2025-08-06 12:38:57 -0300

    feat: add index.ts to export components, utilities, and include CSS styles

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,9 @@
+// Exporting components and types
+export { Button } from "@/components/button";
+export { Loader } from "@/components/loader";
+
+// Exporting utilities
+export { cn } from "@/helpers/misc";
+
+// This line ensures that the CSS is included in the build
+import "./styles/index.css";
